adfba995ace1056f86644b030ade03b70fca5e90
B1mb4__ <b1mb4-art@users.noreply.github.com>
2025-12-31 02:34:11 +0700

    Убран лишний код

--- systems/camera.py
+++ systems/camera.py
@@ -6,25 +6,20 @@ import config
 class GameCamera:
     """Камера для следования за игроком"""
 
-    def __init__(self, width, height):
+    def __init__(self) -> None:
         self.camera = arcade.camera.Camera2D()
         self.viewport_margin = config.VIEWPORT_MARGIN
 
-    def use(self):
+    def use(self) -> None:
         self.camera.use()
 
-    def center_on_sprite(self, sprite, speed=0.1):
+    def center_on_sprite(self, sprite: arcade.Sprite, speed: float=0.1) -> None:
         target_position = (sprite.center_x, sprite.center_y)
+
+        # Перемещаем камеру к позиции спрайта
         self.camera.position = arcade.math.lerp_2d(
             self.camera.position,
             target_position,
-            speed
-        )
-
-    def move_to(self, position, speed=0.1):
-        self.camera.position = arcade.math.lerp_2d(
-            self.camera.position,
-            position,
             speed
         )
 
