af7d06fdb4a9f8c9e9f7269bf303c1a62a06433d
TonyPodd <a_ntony@mail.ru>
2025-12-31 17:41:25 +0700

    Поправил движения

--- entities/player.py
+++ entities/player.py
@@ -14,8 +14,6 @@ class Player(arcade.Sprite):
         self.center_y = y
 
         self.max_speed = config.PLAYER_MOVEMENT_SPEED
-        self.acceleration = config.PLAYER_ACCELERATION
-        self.friction = config.PLAYER_FRICTION
 
         # Направление движения
         self.direction = {
@@ -25,45 +23,33 @@ class Player(arcade.Sprite):
             'down': False
         }
 
+        # Последнее нажатое направление
+        self.last_direction_x = None  # 'left' или 'right'
+        self.last_direction_y = None  # 'up' или 'down'
+
     def update(self) -> None:
-        
-        if self.direction['left']:
-            move_direction_x = -1
+        move_direction_x = 0
+        if self.direction['right'] and self.direction['left']:
+            # Обе клавиши нажаты - используем последнюю
+            if self.last_direction_x == 'right':
+                move_direction_x = 1
+            elif self.last_direction_x == 'left':
+                move_direction_x = -1
         elif self.direction['right']:
             move_direction_x = 1
-        else:
-            move_direction_x = 0
+        elif self.direction['left']:
+            move_direction_x = -1
 
-        if self.direction['down']:
-            move_direction_y = -1
+        move_direction_y = 0
+        if self.direction['up'] and self.direction['down']:
+            if self.last_direction_y == 'up':
+                move_direction_y = 1
+            elif self.last_direction_y == 'down':
+                move_direction_y = -1
         elif self.direction['up']:
             move_direction_y = 1
-        else:
-            move_direction_y = 0
+        elif self.direction['down']:
+            move_direction_y = -1
 
-        # Применяем ускорение в направлении движения
-        if move_direction_x != 0:
-            self.change_x += move_direction_x * self.acceleration
-        else:
-            # Применяем трение когда нет ввода
-            self.change_x *= (1 - self.friction)
-
-        if move_direction_y != 0:
-            self.change_y += move_direction_y * self.acceleration
-        else:
-            # Применяем трение когда нет ввода
-            self.change_y *= (1 - self.friction)
-
-        # Ограничиваем максимальную скорость
-        if abs(self.change_x) > self.max_speed:
-            self.change_x = self.max_speed if self.change_x > 0 else -self.max_speed
-        if abs(self.change_y) > self.max_speed:
-            self.change_y = self.max_speed if self.change_y > 0 else -self.max_speed
-
-        # Останавливаем полностью если скорость очень маленькая
-        if abs(self.change_x) < 0.1:
-            self.change_x = 0
-        if abs(self.change_y) < 0.1:
-            self.change_y = 0
-
-        # Позиция обновляется PhysicsEngine
+        self.change_x = move_direction_x * self.max_speed
+        self.change_y = move_direction_y * self.max_speed
