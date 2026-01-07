e8e4ef3d24d5c7345c9561405b8f6ccddabda7c2
B1mb4__ <b1mb4-art@users.noreply.github.com>
2026-01-07 15:56:32 +0700

    Добавлена функция для загрузки комнаты. Добавлена основа для функции загрузки локации

--- entities/wall.py
+++ entities/wall.py
@@ -7,5 +7,8 @@ class Wall(arcade.Sprite):
     def __init__(self, path_or_texture=None, scale=1, center_x=0, center_y=0):
         super().__init__(path_or_texture, scale, center_x, center_y)
         
+        # Временный квадрат вместо спрайта
+        self.texture = arcade.make_soft_square_texture(TILE_SIZE, arcade.color.GRAY, outer_alpha=255)
+        
         self.width = TILE_SIZE
         self.height = TILE_SIZE
--- scripts/level.py
+++ scripts/level.py
@@ -1,15 +1,42 @@
 import arcade
 import csv
+import random
 
 from entities import Wall, Floor
 from config import *
 
 
-def load_level(name: str) -> dict:
+def load_level(level_type: str='start', num_room: int=2) -> dict:
+    """
+    Загрузка локации \n
+
+    level_type - название локации \n
+    num_room - количество комнат на уравле, не считая лифт и комнату босса
+    """
+    # Спрайты всех объектов комнаты
+    object_data = {
+        'floor': arcade.SpriteList(),
+        'wall': arcade.SpriteList(),
+    }
+    
+    # загрузка спрайтов с лифта
+    
+    return object_data
+
+
+def load_room(name: str, room_type: str='fight', dx: float=0.0, dy: float=0.0) -> dict:
+    """
+    Загрузка комнат локации \n
+
+    name - название комнаты \n
+    room_type - тип комнаты (boss/fight/loot) \n
+    dx - расположение комнаты на карте по X \n
+    dy - расположение комнаты на карте по X \n
+    """
     text_map = list()
     
     # Загружаем информацию с файла
-    with open(file=f'levels/{name}.csv', mode='r', encoding='UTF-8') as file:
+    with open(file=f'levels/{room_type}/{name}.csv', mode='r', encoding='UTF-8') as file:
         reader = csv.reader(file, delimiter=',')
         
         for line in reader:
@@ -24,8 +51,8 @@ def load_level(name: str) -> dict:
     # загружаем объекты с локации
     for row in range(len(text_map)):
         for col in range(len(text_map[row])):
-            tile_x = col * TILE_SIZE
-            tile_y = row * TILE_SIZE
+            tile_x = col * TILE_SIZE + dx
+            tile_y = row * TILE_SIZE + dy
             
             # Пол
             if text_map[row][col] == '1':
