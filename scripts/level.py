94e03c53261b2badf49469c1e5afcf09652d1843
B1mb4__ <b1mb4-art@users.noreply.github.com>
2026-01-22 00:02:23 +0700

    Вместо функции с загрузкой уровней добевлен класс уровня

--- scripts/level.py
+++ scripts/level.py
@@ -6,88 +6,26 @@ from entities import Wall, Floor
 from config import *
 
 
-def load_level(level_type: str='start', num_room: int=2) -> dict:
+def load_level(level_type: str) -> dict:
     """
     Загрузка локации \n
 
     level_type - название локации \n
-    num_room - количество комнат на уравле, не считая лифт и комнату босса
     """
+
     # Спрайты всех объектов комнаты
     object_data = {}
 
-    # загрузка спрайтов с лифта
-    objects_from_room = load_room('elevator', 'elevator', 0, 0)
-    for key in objects_from_room:
-        if key not in object_data:
-            object_data[key] = arcade.SpriteList()
-        for sprite in objects_from_room[key].sprite_list:
-            object_data[key].append(sprite)
-    
-    # загрузка спрайтов с комнаты босса
-    objects_from_room = load_room('test', 'boss', 700, 700)
-    for key in objects_from_room:
-        if key not in object_data:
-            object_data[key] = arcade.SpriteList()
-        for sprite in objects_from_room[key].sprite_list:
-            object_data[key].append(sprite)
-    
-    # загрузка спрайтов с комнат
-
-    return object_data
-
-
-def load_room(name: str, room_type: str='fight', dx: float=0.0, dy: float=0.0) -> dict:
-    """
-    Загрузка комнат локации \n
-
-    name - название комнаты \n
-    room_type - тип комнаты (boss/fight/loot) \n
-    dx - расположение комнаты на карте по X \n
-    dy - расположение комнаты на карте по X \n
-    """
+    # Карта чанков
     text_map = list()
+    for i in range(CHUNCK_SIZE[1]):
+        text_map.append(list())
+        for j in range(CHUNCK_SIZE[0]):
+            text_map[i].append(0)
+            # 0 - Пустой чанк
     
-    # Загружаем информацию с файла
-    with open(file=f'levels/{room_type}/{name}.csv', mode='r', encoding='UTF-8') as file:
-        reader = csv.reader(file, delimiter=',')
-        
-        for line in reader:
-            text_map.append(line)
-        text_map = text_map[::-1]
-    
-    # Спрайты всех объектов комнаты
-    object_data = {
-        'floor': arcade.SpriteList(),
-        'wall': arcade.SpriteList(),
-    }
-    
-    # загружаем объекты с локации
-    for row in range(len(text_map)):
-        for col in range(len(text_map[row])):
-            tile_x = col * TILE_SIZE + dx
-            tile_y = row * TILE_SIZE + dy
-            
-            # Пол
-            if text_map[row][col] == '1':
-                object_data['floor'].append(
-                    Floor(
-                        None,
-                        1,
-                        tile_x,
-                        tile_y
-                    )
-                )
-            
-            # стены
-            if text_map[row][col] == '2':
-                object_data['wall'].append(
-                    Wall(
-                        None,
-                        1,
-                        tile_x,
-                        tile_y
-                    )
-                )
-    
-    return object_data
+    all_rooms = create_rooms(text_map)  # все комнаты
+
+
+def create_rooms(text_map: list[list]) -> dict:
+    all_rooms = dict()  # номер комнаты: класс комнаты
