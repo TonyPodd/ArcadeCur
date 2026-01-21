f057e564c5938c795daddf3ad21ea53b6b2ad11d
B1mb4__ <b1mb4-art@users.noreply.github.com>
2026-01-22 00:49:22 +0700

    Добавлены основы под функции комнат

--- levels/room.py
+++ levels/room.py
@@ -3,5 +3,17 @@ import arcade
 from config import *
 
 class Room:
-    def __init__(self):
+    def __init__(self, room_type: str, room_number: int, x: int, y: int):
+        self.x = x
+        self.y = y
+        self.room_type = room_type
+        self.room_number = room_number
+
+    def get_sprites(self):
+        pass
+
+    def create_door(self):
+        pass
+
+    def create_wall(self):
         pass
