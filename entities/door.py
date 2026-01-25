640ff14e1c0410cc5e5f6ed65d0b40e18a5603c2
B1mb4__ <b1mb4-art@users.noreply.github.com>
2026-01-26 00:58:23 +0700

    Убран лишний код в Door

--- entities/door.py
+++ entities/door.py
@@ -1,11 +1,13 @@
 import arcade
 
+from config import *
+
 
 class Door(arcade.Sprite):
-    def __init__(self, scale=1, center_x=0, center_y=0, size: list[int, int]=None):
+    def __init__(self, scale=1, center_x=0, center_y=0):
         super().__init__(None, scale, center_x, center_y)
         
-        self.width = size[0]
-        self.height = size[1]
+        self.width = TILE_SIZE
+        self.height = TILE_SIZE
         
         self.is_close = False  # Закртыта ли дверь
