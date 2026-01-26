78667378692205927071ba9ad61b32a0555e7912
B1mb4__ <b1mb4-art@users.noreply.github.com>
2026-01-27 00:38:31 +0700

    Добавлены основы в комнаты

--- rooms/spawn_room.py
+++ rooms/spawn_room.py
@@ -6,3 +6,8 @@ from .room import Room
 class SpawnRoom(Room):
     def __init__(self, room_type, room_number, x, y, rooms_coords):
         super().__init__(room_type, room_number, x, y, rooms_coords)
+        
+        object_data = self.data_from_file('spawn')
+
+    def create_spawn(self):
+        pass
