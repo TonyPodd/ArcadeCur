27049672940f01ca78b87eb8f32bee7721d8acc4
B1mb4__ <b1mb4-art@users.noreply.github.com>
2026-01-27 00:50:20 +0700

    Добавлены функции создания спавна и получения спрайта спавна

--- rooms/spawn_room.py
+++ rooms/spawn_room.py
@@ -1,13 +1,21 @@
 import arcade
 
 from .room import Room
-
+from config import *
+from entities import Floor
 
 class SpawnRoom(Room):
     def __init__(self, room_type, room_number, x, y, rooms_coords):
         super().__init__(room_type, room_number, x, y, rooms_coords)
+        self.create_spawn()
         
-        object_data = self.data_from_file('spawn')
+        sprites_from_data = self.data_from_file('spawn')
+        self.add_new_sprites(sprites_from_data)
 
-    def create_spawn(self):
-        pass
+    def create_spawn(self) -> None:
+        spawn_x = self.x * CHUNCK_SIZE[0] * TILE_SIZE + (CHUNCK_SIZE[0] * TILE_SIZE) // 2
+        spawn_y = self.y * CHUNCK_SIZE[1] * TILE_SIZE + (CHUNCK_SIZE[1] * TILE_SIZE) // 2
+        self.spawn = Floor(None, 1, spawn_x, spawn_y)
+    
+    def get_spawn(self):
+        return self.all_sprites['spawn']
