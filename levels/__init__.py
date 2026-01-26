8d8e154a0312f0c89b66614bed3b344f6c8cc6fd
B1mb4__ <b1mb4-art@users.noreply.github.com>
2026-01-26 09:30:52 +0700

    Добавлены классы для ризных видов комнат

--- levels/__init__.py
+++ levels/__init__.py
@@ -1,1 +1,6 @@
 from .room import Room
+from .fight_room import FightRoom
+from .boss_room import BossRoom
+from .loot_room import LootRoom
+from .shop_room import ShopRoom
+from .spawn_room import SpawnRoom
---
+++ levels/fight_room.py
@@ -0,0 +1,8 @@
+import arcade
+
+from .room import Room
+
+
+class FightRoom(Room):
+    def __init__(self, room_type, room_number, x, y, rooms_coords):
+        super().__init__(room_type, room_number, x, y, rooms_coords)
