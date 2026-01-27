bf9155ccf4463af206137d01598bce726e562e7f
B1mb4__ <b1mb4-art@users.noreply.github.com>
2026-01-27 15:03:39 +0700

    Добавлена функция load_sprites_from_data в SpwanRoom для изменений

--- rooms/spawn_room.py
+++ rooms/spawn_room.py
@@ -2,14 +2,15 @@ import arcade
 
 from .room import Room
 from config import *
-from entities import Floor
+from entities import Floor, Wall
 
 class SpawnRoom(Room):
     def __init__(self, room_type, room_number, x, y, rooms_coords):
         super().__init__(room_type, room_number, x, y, rooms_coords)
         self.create_spawn()
         
-        sprites_from_data = self.data_from_file('spawn')
+        data = self.data_from_file('spawn')
+        sprites_from_data = self.load_sprites_from_data(data)
         self.add_new_sprites(sprites_from_data)
 
     def create_spawn(self) -> None:
@@ -19,3 +20,29 @@ class SpawnRoom(Room):
     
     def get_spawn(self) -> arcade.Sprite:
         return self.spawn
+
+    def load_sprites_from_data(self, data: dict) -> dict:
+        # Все спрайты с комнат
+        sprites = {
+            'floor': arcade.SpriteList(),
+            'wall': arcade.SpriteList(), 
+        }
+        
+        for object_type in data:
+            for j, i in data[object_type]:
+                tile_x = self.x * CHUNCK_SIZE[0] * TILE_SIZE + TILE_SIZE * (j + 1)
+                tile_y = self.y * CHUNCK_SIZE[1] * TILE_SIZE + TILE_SIZE * (i + 1)
+            
+                # Пол
+                if object_type == '1':
+                    sprites['floor'].append(Floor(
+                        None, 1, tile_x, tile_y
+                    ))
+                
+                #  Стена
+                if object_type == '2':
+                    sprites['wall'].append(Wall(
+                        None, 1, tile_x, tile_y
+                    ))
+        
+        return sprites
