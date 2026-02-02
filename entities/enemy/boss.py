1a33cdc0e6f93b9fc81f62d98b0f307c9984f692
B1mb4__ <b1mb4-art@users.noreply.github.com>
2026-02-02 08:42:39 +0700

    enemy.py перемещён в отдельную папку

---
+++ entities/enemy/boss.py
@@ -0,0 +1,15 @@
+import arcade
+
+
+class Boss(arcade.Sprite):
+    def __init__(self, path_or_texture = None, scale = 1, center_x = 0, center_y = 0):
+        super().__init__(path_or_texture, scale, center_x, center_y)
+        
+    def update(self, delta_time=1 / 60):
+        ...
+
+    def draw(self):
+        ...
+
+    def update(self):
+        ...
