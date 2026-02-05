4699ee9edef64ecc5c6f86fc6d831565e1761bb3
B1mb4__ <b1mb4-art@users.noreply.github.com>
2026-02-06 00:49:12 +0700

    Отдельная функция для setup настроек звуков

--- views/settings_menu.py
+++ views/settings_menu.py
@@ -15,27 +15,21 @@ class SettingsMenu(arcade.View):
         self.manager = gui.UIManager()
         self.manager.enable()
 
-        self.anchor_layout = gui.UIAnchorLayout()
         self.anchor_settings = gui.UIAnchorLayout()
-        
-        self.box_layout = gui.UIBoxLayout(vertical=True, space_between=10)
-        self.settigns_layout = gui.UIBoxLayout(vertical=True, space_between=10)
+        self.settigns_layout = gui.UIBoxLayout(vertical=True, space_between=10, height= 600)
         
         self.button_width = 200
         
         self.setup_widgets()
         
-        self.anchor_layout.add(self.box_layout)
         self.anchor_settings.add(self.settigns_layout)
-        self.settigns_layout
-        self.manager.add(self.anchor_layout)
         self.manager.add(self.anchor_settings)
 
     def load_settings_from_file(self):
         with open(file='settings.json', mode='r', encoding='utf-8') as file:
             data = json.load(file)
 
-        self.volume_sound = data['sound_volume']
+        self.settings = data
 
     def setup_widgets(self):
         # buttons
@@ -47,42 +41,62 @@ class SettingsMenu(arcade.View):
             font_size=25,
             bold=True,
         )
-        self.box_layout.add(menu_text)
+        self.settigns_layout.add(menu_text)
 
-        # остальные кнопки
-        ...
-        
+        # settigns
+        self.setup_sounds()
+
         # Сохранить & нажать
         lower_layout = gui.UIBoxLayout(vertical=False, space_between=10)  # layout для кнопок сохранить и назал
+
         # Назад
         back_button = gui.UIFlatButton(
             text="Назад", width=self.button_width
         )
         back_button.on_click = self.go_back
         lower_layout.add(back_button)
+
         # save button
         save_button = gui.UIFlatButton(
             text="Сохранить", width=self.button_width
         )
         save_button.on_click = self.save_settigns
         lower_layout.add(save_button)
-        self.box_layout.add(lower_layout)
-    
-        # settigns
-        # Громкасть звуков
+
+        self.settigns_layout.add(lower_layout)
+
+    def setup_sounds(self):
+        """ Раздел звука """
+        # Звуки
+        sound_layout = gui.UIBoxLayout(vertical=True, space_between=10)
+
+        # текст раздела
+        text = gui.UITextArea(
+            text="Звуки", 
+            width=100,
+            height=40,
+            font_size=20,
+            bold=True
+        )
+        sound_layout.add(text)
+
+        # Громкасть звуков игры
         volume_layout = gui.UIBoxLayout(vertical=False)
         volume_text = gui.UITextArea(
-            text="Звуки:", 
+            text="SFX:", 
             width=60,
             height=20,
             font_size=14,
         )
         volume_layout.add(volume_text)
+        
+        #Ползунок звуков
         self.volume_slider = gui.UISlider(
-            width=200, height=20, min_value=0, max_value=100, value=100 * self.volume_sound
+            width=200, height=20, min_value=0, max_value=100, value=100 * self.settings['sound_volume']
         )
         volume_layout.add(self.volume_slider)
-        self.settigns_layout.add(volume_layout)
+        sound_layout.add(volume_layout)
+        self.settigns_layout.add(sound_layout)
 
     def on_draw(self):
         self.clear()
